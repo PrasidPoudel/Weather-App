ecd8582cf7ea8a44b41c71b23cca80866d793947
PrasidPoudel <prasidpoudel7.com>
2025-05-28 14:14:50 +0545

    Refactored the code by adding date-fns

--- src/script/DOMs.js
+++ src/script/DOMs.js
@@ -1,34 +1,23 @@
 const DOM = (function () {
-    MainSection(address, date, icon, tempFeels) {
-        const City = document.getElementsByClassName('city-name') //City Info is here
-        const day = document.getElementsByClassName('day') //Day Info is here
-        const emoji = document.getElementsByClassName('emoji-mainTemp')
-        const weatherInfo = document.getElementsByClassName('weatherInfo')
-        const temp = document.getElementsByClassName('section-3')
+    function MainSection(address, date, /*icon,*/ tempFeels) {
+        const City = document.querySelector('.city-name'); // City Info is here
+        const day = document.querySelector('.day'); // Day Info is here
+        const emoji = document.querySelector('.emoji-mainTemp');
+        const weatherInfo = document.querySelector('.weatherInfo');
+        const temp = document.querySelector('.section3');
+
+        console.log(City, day, temp)
         //Now let the DOM get their corresponding value
         City.textContent = address
-        day.textContent = new Date(date).getDay
+        day.textContent = date
+        temp.textContent = tempFeels
+    };
+
+    return {
+        MainSection
     }
 })();
 
-function GetDay(date) {
-    let num = new Date(date).getDay
-    switch (num) {
-        case 0:
-            return 'Sunday'
-        case 1:
-            return 'Monday'
-        case 2:
-            return 'Tuesday'
-        case 3:
-            return 'Wednesday'
-        case 4:
-            return 'Thursday'
-        case 5:
-            return 'Friday'
-        case 6:
-            return 'Saturday'
-        default:
-            return 'Error'
-    }
+export {
+    DOM
 }

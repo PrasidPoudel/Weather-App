fe3294dfcf4bbd8ee480a2eee4961e46fd929b4a
PrasidPoudel <prasidpoudel7.com>
2025-05-31 14:03:32 +0545

    Added Celsius and Farenheit Option

--- src/script/index.js
+++ src/script/index.js
@@ -2,21 +2,51 @@ import "../style.css"
 import {
     DOM
 } from "./DOMs"
-import { format } from "date-fns";
+import {
+    format
+} from "date-fns";
 //Let's Get The Data of City from SearchBar
 const SearchButton = document.querySelector('form>button')
 const SearchBar = document.getElementById('searchbox')
+const loader = document.querySelector('.loader')
+const sections = document.querySelectorAll('.section')
+const footer = document.querySelector('.footer')
+const units = document.querySelectorAll('.unit-child')
+units.forEach((unit) => {
+    unit.addEventListener('click', () => {
+        const another = document.querySelector('.active')
+        another.classList.remove('active')
+        unit.classList.add('active')
+    })
+})
 
 SearchButton.addEventListener('click', (event) => {
     event.preventDefault()
-     GetData(SearchBar.value).then((res) => {
+    sections.forEach((section) => section.style.display = 'none')
+    footer.style.display = 'none'
+    document.querySelector('.main-section').style.borderColor = 'transparent'
+    loader.style.display = 'block'
+
+    GetData(SearchBar.value).then((res) => {
         console.log(res)
-        DOM.MainSection(res.address, format(new Date(), 'EEEE'),res.currentConditions.icon,res.currentConditions.feelslike)
-        for(let i=1;i<=6;i++) {
-            let array=res.days[i]
-            DOM.WeekDOM(array.icon,array.datetime,array.feelslike,i)
+        loader.style.display = 'none'
+        DOM.MainSection(res.address, format(new Date(), 'EEEE'), res.days[0].icon, res.days[0].temp, res.currentConditions.feelslike, res.days[0].tempmin)
+        for (let i = 1; i <= 6; i++) {
+            let array = res.days[i]
+            DOM.WeekDOM(array.icon, array.datetime, array.feelslike, i)
         }
-    }).catch((error)=>alert(error))
+        units.forEach((unit) => {
+            unit.addEventListener('click', () => {
+                console.log(unit)
+                DOM.MainSection(res.address, format(new Date(), 'EEEE'), res.days[0].icon,res.currentConditions.feelslike, res.days[0].tempmax, res.days[0].tempmin)
+                for (let i = 1; i <= 6; i++) {
+                    let array = res.days[i]
+                    DOM.WeekDOM(array.icon, array.datetime, array.feelslike, i)
+                }
+            })
+        })
+
+    }).catch((error) => alert(error))
 })
 
 
